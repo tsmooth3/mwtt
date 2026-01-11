f51d97a5ecd11203b333df61738f2816b6690e0c
Tim Stewart <tim@timii.tech>
2026-01-11 00:49:48 -0500

    fix docker build

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.credentials.oauth[:google_client_id], Rails.application.credentials.oauth[:google_client_secret]
+  # Only configure Google OAuth if credentials are available
+  # This allows asset precompilation to work without credentials during Docker build
+  begin
+    oauth_creds = Rails.application.credentials.dig(:oauth)
+    if oauth_creds && oauth_creds[:google_client_id] && oauth_creds[:google_client_secret]
+      provider :google_oauth2, 
+               oauth_creds[:google_client_id], 
+               oauth_creds[:google_client_secret]
+    end
+  rescue NoMethodError, TypeError
+    # Credentials not available (e.g., during Docker build without master key)
+    # OAuth will be unavailable, but app can still run
+  end
 end
 
 OmniAuth.config.allowed_request_methods = [ :post, :get ]
