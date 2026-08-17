a3f9998328afddf2491febf449a09bc1cdfba62e
kid stew <kids@extrapants.com>
2026-08-17 19:25:06 -0400

    Fix leftover scaffold tests and make CI use the local Ruby version
    Scaffold controller tests called URL helpers that never existed.
    Point CI at committed .ruby-version and SQLite so the suite can run.

--- test/controllers/families_controller_test.rb
+++ test/controllers/families_controller_test.rb
@@ -1,23 +1,35 @@
 require "test_helper"
 
 class FamiliesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get families_index_url
+  setup do
+    sign_in users(:one)
+  end
+
+  test "lists families" do
+    get families_url
+
+    assert_response :success
+    assert_match families(:one).name, response.body
+  end
+
+  test "shows the new family form" do
+    get new_family_url
+
     assert_response :success
   end
 
-  test "should get new" do
-    get families_new_url
-    assert_response :success
+  test "creates a family" do
+    assert_difference("Family.count", 1) do
+      post families_url, params: { family: { name: "Birch Lane" } }
+    end
+
+    assert_redirected_to family_url(Family.order(:id).last)
   end
 
-  test "should get create" do
-    get families_create_url
-    assert_response :success
-  end
+  test "shows a family" do
+    get family_url(families(:one))
 
-  test "should get show" do
-    get families_show_url
     assert_response :success
+    assert_match families(:one).name, response.body
   end
 end
--- test/controllers/season_goals_controller_test.rb
+++ test/controllers/season_goals_controller_test.rb
@@ -1,13 +1,26 @@
 require "test_helper"
 
 class SeasonGoalsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get season_goals_create_url
-    assert_response :success
+  setup do
+    family_memberships(:one).update!(is_admin: true)
+    sign_in users(:one)
   end
 
-  test "should get update" do
-    get season_goals_update_url
-    assert_response :success
+  test "creates a season goal" do
+    SeasonGoal.where(season: seasons(:season_2024)).delete_all
+
+    post season_goals_url, params: { season_id: 2024, goal_count: 180 }
+
+    assert_redirected_to dashboard_path(season_id: 2024)
+    assert_equal 180, SeasonGoal.find_by!(season: seasons(:season_2024)).goal_count
+  end
+
+  test "updates a season goal" do
+    patch season_goal_url(season_goals(:season_2025)), params: {
+      season_goal: { goal_count: 300 }
+    }
+
+    assert_redirected_to dashboard_path(season_id: 2025)
+    assert_equal 300, season_goals(:season_2025).reload.goal_count
   end
 end
